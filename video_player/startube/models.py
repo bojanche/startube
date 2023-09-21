f07c1d92fe450b76bb88ced1b9d7353a456653db
Bojan <32612389+bojanche@users.noreply.github.com>
2023-09-21 14:38:56 +0200

    102

--- video_player/startube/models.py
+++ video_player/startube/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class Asset(models.Model):
+    asset_name = models.CharField(max_length=500)
+    asset_type = models.CharField(max_length=20, blank=True)
+    asset_upload_timestamp = models.DateTimeField(auto_now=True)
+    asset_original = models.BooleanField(default=True)
+    # asset_owner = models
+    asset_dimension_x = models.IntegerField(null=True)
+    asset_dimension_y = models.IntegerField(null=True)
+    asset_duration = models.FloatField(null=True)
+    asset_codec = models.CharField(max_length=20, blank=True)
